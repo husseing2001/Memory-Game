8923a27abd34a82113183e2a5a17fc669bf54945
husse <hussein.g.abouzeid@gmail.c>
2023-01-30 12:07:06 -0500

    initial commit

---
+++ app/src/main/java/com/example/mymemory/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.example.mymemory.utils
+
+import com.example.mymemory.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_construction,
+    R.drawable.ic_face,
+    R.drawable.ic_delivery,
+    R.drawable.ic_girl,
+    R.drawable.ic_list,
+    R.drawable.ic_microphone,
+    R.drawable.ic_monitor,
+    R.drawable.ic_moon,
+    R.drawable.ic_seat,
+    R.drawable.ic_shower,
+    R.drawable.ic_sick,
+    R.drawable.ic_yoga,
+)
